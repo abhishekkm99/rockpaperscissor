479e96e65e6d8a825737e5601bd280bfef2e3c88
abhishekkm99 <89724622+abhishekkm99@users.noreply.github.com>
2022-08-28 23:22:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
 /*
   Rock Paper Scissors 🚀🔥
-  Concepts covered in this project
-    👉 For loops
-    👉 Dom Manipulation
-    👉 Variables
-    👉 Conditionals (if else if)
-    👉 Template Literals
-    👉 Event Listeners
-    👉 Higher order Function (Math.random())
 */
 
 let totalScore={computerScore:0 ,playerScore:0}
 
 // ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
-// getComputerChoice() 👉 'Rock'
-// getComputerChoice() 👉 'Scissors'
 function getComputerChoice() {
     const rpsChoice = ['Rock', 'Paper', 'Scissors']
     const randomChoice = Math.floor(Math.random() * 3)
     return rpsChoice[randomChoice]
 }
 // ** getResult compares playerChoice & computerChoice and returns the score accordingly **
-// human wins - getResult('Rock', 'Scissors') 👉 1
-// human loses - getResult('Scissors', 'Rock') 👉 -1
-// human draws - getResult('Rock', 'Rock') 👉 0
 function getResult(playerChoice, computerChoice) {
+  
     // return the result of score based on if you won, drew, or lost
-
-
+  
     let score;
     // All situations where human draws, set `score` to 0
     if (playerChoice === computerChoice) {
         score = 0
-        // All situations where human wins, set `score` to 1
-        // make sure to use else ifs here
+       
     } 
+   // All situations where human wins, set `score` to 1
     else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
         score = 1
     } 
@@ -48,7 +35,6 @@ function getResult(playerChoice, computerChoice) {
     else {
         score = -1
     }
-    // return score
     return score;
 
 
@@ -56,13 +42,11 @@ function getResult(playerChoice, computerChoice) {
 
 // ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
 function showResult(score, playerChoice, computerChoice) {
-    // Hint: on a score of -1
-    // You should do result.innerText = 'You Lose!'
-    // Don't forget to grab the div with the 'result' id!
-
+ 
     const resultDiv= document.getElementById('result');
     const playerScoreDiv = document.getElementById('player-score');
     const handsDiv = document.getElementById('hands');
+  
     if (score === -1) {
         resultDiv.innerText= 'You Lose!'
     }
@@ -80,16 +64,10 @@ function showResult(score, playerChoice, computerChoice) {
 
 // ** Calculate who won and show it on the screen **
 function onClickRPS(playerChoice) {
-    // console.log({playerChoice})
     const computerChoice = getComputerChoice()
-    // console.log({computerChoice});
     const score =getResult(playerChoice, computerChoice)
-    // console.log({score});
-    // return score
     totalScore['playerScore'] +=score
     showResult(score, playerChoice, computerChoice)
-
-
 }
 
 
@@ -97,22 +75,13 @@ function onClickRPS(playerChoice) {
 function playGame() {
     // use querySelector to select all RPS Buttons
     const  rpsButtons = document.querySelectorAll('.rpsButton');
-
-    // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
-
-    // 1. loop through the buttons using a forEach loop
-    // 2. Add a 'click' event listener to each button
-    // 3. Call the onClickRPS function every time someone clicks
-    // 4. Make sure to pass the currently selected rps button as an argument
+  
     rpsButtons.forEach(rpsButton => {
         rpsButton.onclick =()=>{
             onClickRPS(rpsButton.value)
         }
     })
-
-
-
-    // Add a click listener to the end game button that runs the endGame() function on click
+  
     const endGameButton = document.getElementById('endGameButton')
     endGameButton.onclick = ()=> endGame(totalScore)
 }
@@ -120,7 +89,7 @@ function playGame() {
 // ** endGame function clears all the text on the DOM **
 function endGame(totalScore) {
     totalScore['playerScore']= 0
-    totalScore['computerScore']= 0
+    // totalScore['computerScore']= 0
 
     const resultDiv = document.getElementById('result')
     const handsDiv = document.getElementById('hands')
